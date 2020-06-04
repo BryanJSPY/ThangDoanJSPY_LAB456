388ff9332e4c558d1d384c51e1d464565a0dd3ea
BryanJSPY <thangdoanjspy@gmail.com>
2020-06-04 15:59:21 +0700

    Add Lab456 Part1

---
+++ doanchithang_lab456_1711061719/doanchithang_lab456_1711061719/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="doanchithang_lab456_1711061719.MvcApplication" Language="C#" %>
---
+++ doanchithang_lab456_1711061719/doanchithang_lab456_1711061719/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(doanchithang_lab456_1711061719.Startup))]
+namespace doanchithang_lab456_1711061719
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
